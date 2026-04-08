db493e2474d935df7254f8c63d02b86c934c50ac
Adithka <151950876+ADITH-K-A@users.noreply.github.com>
2026-04-08 12:21:40 +0530

    fetch images on homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import { useLocation, useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import resume from "./resume";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -15,8 +15,11 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { auth, isLoading } = usePuterStore();
+  const { auth, kv,isLoading } = usePuterStore();
   const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
 
   useEffect(() => {
     if (!isLoading && !auth.isAuthenticated) {
@@ -27,6 +30,21 @@ export default function Home() {
   if (isLoading) return null;
 
   if (!auth.isAuthenticated) return null;
+  
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+      const resumes = (await kv.list('resume:*',true)) as KVItem[];
+      const parsedResumes = resumes?.map((resume) => (
+        JSON.parse(resume.value) as Resume
+      ));
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false); 
+    }
+
+    loadResumes();
+  })
+
 
   return (
     <main className="bg-[url('/images/bg-main.svg')]">
@@ -34,14 +52,31 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Application & Resume Rating</h1>
-          <h2>Review your submissions and check Ai-powered feedback</h2>
+          {!loadingResumes && resumes.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get started!</h2>
+          ):(
+            <h2>Review your submissions and check Ai-powered feedback</h2>
+          )}
         </div>
+        {!loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" alt="" />
+          </div>
+        )}
 
-        {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
+          </div>
+        )}
+
+        {!loadingResumes && resumes.length === 0 &&  (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
           </div>
         )}
       </section>
